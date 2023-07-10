f340f7276fce3def925b0e6829a5980defebc6f1
Hassaan Qadir <hassaanqadir1@gmail.com>
2023-07-10 14:47:26 +0000

    change time message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -122,7 +122,7 @@ function App() {
       <header className="App-header">
       <form onSubmit={handleSubmit}>
       <input className="App-input" type="text" value={input} onChange={e => setInput(e.target.value)} />
-      <button className="App-button" type="submit">{isSubmitted ? 'Submitted' : 'Outline and automate your bio research project now (Results return in ~30 seconds)'}</button>
+      <button className="App-button" type="submit">{isSubmitted ? 'Submitted' : 'Outline and automate your bio research project now (Results return in ~3 minutes)'}</button>
       </form>
 
       {isLoading && <div>{loadingMessage}</div>}
