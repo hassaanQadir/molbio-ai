da5cf8e5d8458b4a8772ce51d40114da5813218e
Hassaan Qadir <hassaanqadir1@gmail.com>
2023-07-10 15:35:15 +0000

    fix import error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 
 function JSONDisplay({ data, level = 0 }) {
   const createMarkup = (data) => {
